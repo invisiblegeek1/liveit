aefd64b0218ec815c8e681472559b0561420a0d6
pushparaja <appumoorthy15@gmail.com>
2021-03-11 14:41:23 +0530

    filter bug resolved

--- liveit/src/containers/dataloader.js
+++ liveit/src/containers/dataloader.js
@@ -32,7 +32,7 @@ function Dataloader() {
       {Data.loading ? (
         <CircularProgress />
       ) : (
-        Data.data.map((txt,index)=>{ return filterFunction(txt,index)}))}
+        Data.data.filter(txt=>txt.price>100).map((txt,index)=>{return (<Card key={index} content={txt} />)}))}
     </div>
   );
 }
